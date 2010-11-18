f6bf40e1188c70215a6d9dbe9d19fd166a6135c2
Eric Strom <asg@cpan.org>
2010-11-18 13:35:40 +0000

    Import of ASG/Whatever-0.21 from CPAN.
    gitpan-cpan-distribution: Whatever
    gitpan-cpan-version:      0.21
    gitpan-cpan-path:         ASG/Whatever-0.21.tar.gz
    gitpan-cpan-author:       ASG
    gitpan-cpan-maturity:     released

--- lib/Whatever.pm
+++ lib/Whatever.pm
@@ -1,53 +1,94 @@
 package Whatever;
     use warnings;
     use strict;
-    use List::Util 'reduce';
-    our ($a, $b);
+    use Carp ();
+    sub new {
+        my $code = shift;
+        bless sub :lvalue {
+            goto &$code if @_ < 2;
+            my $star = $code;
+            {$star = $star->(shift);
+                @_ and ref $star eq 'Whatever' ? redo
+                       : Carp::croak 'too many arguments for Whatever'}
+            $star
+        }
+    }
     use overload fallback => 1,
-        '&{}' => sub {
-            my $self = shift;
-            sub {@_ < 2 ? &$$self : reduce {$a->($b)} $$self, @_}
-        },
-        (map { # binary ops
-            my $code = eval "sub {\$_[0] $_ \$_[1]}" or die $@;
-            $_ => sub {
-                my ($self, $flip) = @_[0, 2];
-                my $arg2 = \$_[1];
-                bless \sub {
-                    $code->($flip ? ($$arg2, &$self)
-                                  : (&$self, $$arg2))
+        (# infix
+            map {
+                my $code = /atan2/ ? sub {atan2 $_[0], $_[1]}
+                                   : eval "sub {\$_[0] $_ \$_[1]}" or die $@;
+                $_ => sub {
+                    my ($self, $flip) = @_[0, 2];
+                    my $arg2 = \$_[1];
+                    new sub {
+                        $code->($flip ? ($$arg2, &$self)
+                                      : (&$self, $$arg2))
+                    }
                 }
-            }
-        } qw (+ - * / % ** << >> x . & | ^ < <= > >= == != lt le gt
-              ge eq ne <=> cmp ), $^V >= 5.010 ? '~~' : ()),
-        (map { # unary ops
-            my $code = eval "sub {$_ \$_[0]}" or die $@;
-            ($_ eq '-' ? 'neg' : $_) => sub {
-                my $self = $_[0];
-                bless \sub {$code->(&$self)}
-            }
-        } qw (- ! ~)),
-        (map { # functions
-            my $code = eval "sub {$_(\$_[0]".(/2/ ? ',$_[1])}' : ')}') or die $@;
-            $_ => sub {
-                my $self = $_[0];
-                bless \sub {$code->(&$self)}
-            }
-        } qw (atan2 cos sin exp abs log sqrt));
+            } qw (+ - * / % ** << >> x . & | ^ < <= > >= == != lt le gt
+                  ge eq ne <=> cmp atan2), $^V >= 5.010 ? '~~' : ()
+        ),
+        (# prefix
+            map {
+                my $code = eval "sub {$_ \$_[0]}" or die $@;
+                ($_ eq '-' ? 'neg' : $_) => sub {
+                    my $self = $_[0];
+                    new sub {$code->(&$self)}
+                }
+            } qw (- ! ~)
+        ),
+        (# functions
+            map {
+                my $code = eval "sub {$_(\$_[0])}" or die $@;
+                $_ => sub {
+                    my $self = $_[0];
+                    new sub {$code->(&$self)}
+                }
+            } qw (cos sin exp abs log sqrt)
+        ),
+        '@{}' => sub {tie my @ret => 'Whatever::ARRAY', shift; \@ret},
+        '%{}' => sub {tie my %ret => 'Whatever::HASH',  shift; \%ret};
 
-    ** = sub {bless \sub {@_ ? shift : $_}};
-    *@ = sub {bless \sub {shift}};
-    *_ = sub {bless \sub {$_}};
+    ** = sub :lvalue {my $x = new sub :lvalue {@_ ? $_[0] : $_}};
+    *@ = sub {new sub :lvalue {$_[0]}};
+    *_ = sub {new sub :lvalue {$_}};
     ** = \do{&*};
+    eval {Internals::SvREADONLY($*, 1); 1}
+        or warn 'Whatever could not set $* readonly: '.$@;
 
+    sub DESTROY {}
     sub AUTOLOAD {
         my $self = shift;
         my $args = \@_;
         my $method = substr our $AUTOLOAD, 2 + length __PACKAGE__;
-        bless \sub {(&$self)->$method(@$args)}
+        new sub {(&$self)->$method(@$args)}
     }
-    sub DESTROY {}
-    our $VERSION = '0.10';
+    {package
+        Whatever::ARRAY;
+        sub TIEARRAY {bless \\pop}
+        sub FETCH {
+            my ($self, $key) = @_;
+            Whatever::new sub :lvalue {
+                (&$$$self ||= [])->[$key - ($key > 2**30 and 2**31-1)]
+            }
+        }
+        sub FETCHSIZE {2**31-1}
+        sub AUTOLOAD {Carp::croak our $AUTOLOAD . " unsupported"}
+        sub DESTROY {}
+    }
+    {package
+        Whatever::HASH;
+        sub TIEHASH {bless \\pop}
+        sub FETCH {
+            my ($self, $key) = @_;
+            Whatever::new sub :lvalue {(&$$$self ||= {})->{$key}}
+        }
+        sub AUTOLOAD {Carp::croak our $AUTOLOAD . " unsupported"}
+        sub DESTROY {}
+    }
+    delete $Whatever::{new};
+    our $VERSION = '0.21';
 
 =head1 NAME
 
@@ -55,7 +96,7 @@ Whatever - a perl6ish whatever-star for perl5
 
 =head1 VERSION
 
-Version 0.10
+Version 0.21
 
 =head1 SYNOPSIS
 
@@ -80,7 +121,7 @@ can now be:
 =head1 EXPORT
 
     &*  the whatever-star
-    $*  the whatever-star ($* is deprecated in 5.10+, so i'm taking it)
+    $*  the whatever-star         ($* is deprecated in 5.10+, so I'm taking it)
     &@  the gets-val-from-@_-star
     &_  the gets-val-from-$_-star
 
@@ -90,30 +131,37 @@ packages after this module is loaded.
 =head1 SUBROUTINES
 
 the C< &* > and C< $* > stars are the most generic terms, which return their
-expression as a sub that will take its argument from C< $_[0] > if it is
+expression as a coderef that will take its argument from C< $_[0] > if it is
 available, or C< $_ > otherwise. this allows the terms to dwim in most contexts.
 
-the C< &@ > term always uses C< $_[0] >, while the C< &_ > always uses C< $_ >
+the C< &@ > term always uses C< $_[0]>, while the C< &_ > always uses C< $_ >
 
 beyond where they get their eventual argument from, all of the whatever terms
-behave the same way.  each is a 'sticky' overloaded object that will bind to
-the operators and variables that it interacts with.  the end result is a
-subroutine that will perform the operations when passed it's value.
+behave the same way.  each is a I<sticky> overloaded object that will bind to
+the operators and variables that it interacts with.  at all times the whatever
+star is a coderef that will perform the actions it has accumulated when passed
+a value to act on.
 
 a few more examples are probably in order:
+
+=over 4
+
+=item hello world
 
     my $greet = "hello, $*!";  # the $* term interpolates in strings
     say $greet->('world'); # prints 'hello, world!'
 
     say "hello, $*!"->('world');
 
+=item simple operations
+
     my $inc = $* + 1;
     say $inc->(5); # prints 6
 
-    my $inc_2 = $inc * 2;
-    say $inc_2->(5); # prints 12
+    my $inc_x2 = $inc * 2;  # whatever code continues to capture operations
+    say $inc_x2->(5); # prints 12
 
-    my $inc_inc = $inc->($inc);
+    my $inc_inc = $inc->($inc); # and is fine with recursion
     say $inc_inc->(5); # prints 7
 
     my $repeat = &* x &*;
@@ -122,8 +170,12 @@ a few more examples are probably in order:
 
     print $hr->(80);  # prints ('-' x 80)."\n"
 
+=item with object oriented code
+
+assuming this simple C< Array > implementation:
+
     {package Array;
-        sub new  {shift; bless \@_}
+        sub new  {bless \@_, shift}
         sub map  {new Array map  $_[1]() => @{$_[0]}}
         sub grep {new Array grep $_[1]() => @{$_[0]}}
         sub str  {join ' ' => @{$_[0]}}
@@ -134,12 +186,18 @@ a few more examples are probably in order:
     say $array->map(&_ * 2)->map(&_ + 1)->str; # '3 5 7 9 11 13 15 17 19 21'
     say $array->map(&_ * 2 + 1)->str;          # '3 5 7 9 11 13 15 17 19 21'
 
+=item method calls
+
     my $str = &*->str;
     say $str->($array); # prints '1 2 3 4 5 6 7 8 9 10'
 
     my $multi_call = &*->map(&_ * 2 + 1)->grep(&_ % 5)->str;
 
     say $multi_call->($array); # prints '3 7 9 11 13 17 19 21'
+
+    $some_obj->map(&*->some_method(...));
+
+=item multiple whatever stars
 
 when working with subs created by combining multiple stars, you can bind
 multiple values at once by passing multiple arguments.
@@ -153,17 +211,42 @@ multiple values at once by passing multiple arguments.
 
     say $indent->('xyz'); # prints '  xyz'
 
+=item arrays and hashes
+
+you can dereference a whatever star as an array or hash (of course the star
+expects to be passed a suitable reference):
+
+    my $first = &*->[0];
+    my $bob   = &*->{bob};
+
+    say $first->([3 .. 5]); # prints '3'
+    say $bob->({bob => 5}); # prints '5'
+
+the subroutine returned by the star is a valid lvalue (can be assigned to).
+multi-level calls and calls that would normally autovivify behave as expected.
+
+    &*->[0][0]{x}(my $array) = 4;
+
+    say $$array[0][0]{x}; # prints '4'
+
+=item variables
+
 the stars lazily bind to variables, which allows the variable to get its value
-after the star is defined, and to change its value between calls
+after the star is defined, and to change its value between calls.  this is
+analogous to an anonymous sub closing over a variable
 
     my $future;
     my $delorean = $future . (' ' . $* . '!');
+     # works like: sub {$future . (' ' . $_[0] . '!')};
 
     $future = 1.21;
     say $delorean->('gigawatts'); # prints "1.21 gigawatts!"
 
     $future = &*;
     say $delorean->('folks')->("that's all");  # prints "that's all folks!"
+
+
+=back
 
 =head1 AUTHOR
 
@@ -173,7 +256,7 @@ Eric Strom, C<< <asg at cpan.org> >>
 
 this module is new, there are probably some.
 
-Please report any bugs or feature requests to C<bug-whatever at rt.cpan.org>,
+please report any bugs or feature requests to C<bug-whatever at rt.cpan.org>,
 or through the web interface at
 L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Whatever>.  I will be
 notified, and then you'll automatically be notified of progress on your bug as
--- t/whatever.t
+++ t/whatever.t
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Test::Magic tests => 62;
+use Test::Magic tests => 121;
 use lib '../lib';
 use Whatever;
 
@@ -19,6 +19,12 @@ test 'basic',
   is &*->('a') eq 'a',
   map {is &*->() == $_} 3;
 
+test 'too many args', do {
+  my $error_ok = qr/^too many arguments for Whatever.*whatever\.t/s;
+  do {eval {&*->(1..2)};      is $@ == $error_ok},
+  do {eval {(&*.&*)->(1..3)}; is $@ == $error_ok},
+};
+
 my $lhs = &* . 3;
 
 my $rhs = ">$*";
@@ -34,7 +40,7 @@ test 'single rhs',
   map {is $rhs->() eq '>5'} 5;
 
 test 'double',
-  is  ref($lhs . 8) eq 'Whatever',
+  is ref($lhs . 8) eq 'Whatever',
   is +($lhs . 8)->('asdf') eq 'asdf38';
 
 sub plus2 {$_[0] + 2}
@@ -68,10 +74,10 @@ test 'lazy arg',
                             eq "that's all folks"};
 
 {package Array;
-    sub new  {shift; bless \@_}
+    sub new  {bless \@_, shift}
     sub map  {new Array map  $_[1]() => @{$_[0]}}
     sub grep {new Array grep $_[1]() => @{$_[0]}}
-    sub join {join $_[1] => @{$_[0]}}
+    sub join {join $_[1]||'' => @{$_[0]}}
     sub str  {$_[0]->join(' ')}
 }
 
@@ -87,9 +93,20 @@ test 'method call', do {
   my $str = &*->str;
   my $add1 = &*->map(&* + 1);
   my $add1evens = &*->map(&* + 1)->grep(not &* % 2);
-  is $str->($array)               eq '1 2 3 4 5 6 7 8 9 10',
-  is $str->($add1->($array))      eq '2 3 4 5 6 7 8 9 10 11',
-  is $str->($add1evens->($array)) eq '2 4 6 8 10',
+  is $str->($array)                  eq '1 2 3 4 5 6 7 8 9 10',
+  is $str->($add1->($array))         eq '2 3 4 5 6 7 8 9 10 11',
+  is $str->($add1evens->($array))    eq '2 4 6 8 10',
+  is &*->str->($array)               eq '1 2 3 4 5 6 7 8 9 10',
+  is &*->grep(&_ % 2)->str->($array) eq '1 3 5 7 9',
+  is &*->grep(&_ % 2)->($array)->str eq '1 3 5 7 9',
+  is $*->new(1 .. 5)->str->('Array') eq '1 2 3 4 5',
+};
+
+test 'method call inner', do {
+  my $obj = new Array map {new Array 1 .. $_} 1 .. 4;
+  is $obj->map(&*->join)->join(' ') eq '1 12 123 1234',
+  is $*->map('['.&*->join.']')->join->($obj) eq '[1][12][123][1234]',
+  is +('['.$*->map($*->join('-'))->join('][').']')->($obj) eq '[1][1-2][1-2-3][1-2-3-4]',
 };
 
 for (&*) {
@@ -115,65 +132,224 @@ test 'method $*',
 test 'sin($*)',
   is +(5 * sin $*)->(0.5) == 5 * sin 0.5;
 
+test 'atan2',
+  is atan2(&*, 0)->(1)     == atan2(1, 0),
+  is atan2(1, &*)->(0)     == atan2(1, 0),
+  is atan2(&*, &*)->(1, 1) == atan2(1, 1),
+  do {
+    my $xatan2 = &* * atan2 &*, &*;
+    is $xatan2->(4, 1, 1) == $xatan2->(2, 1, 0)
+  };
+
+
 my $not = not &*;
 test 'not',
   is $not->(1) eq ! 1,
   is $not->(0) == ! 0;
 
 test '&* x &*', do {
-    my $rep  = &* x &*;
-    my $line = $rep->('-');
-    my $hr = $line . "\n";
-    is $line->(10)            eq '-' x 10,
-    is $rep->($line->(10))(2) eq '-' x 20,
-    is $rep->($line)(3)(10)   eq '-' x 30,
-    is $hr->(20)              eq '-' x 20 . "\n",
+  my $rep  = &* x &*;
+  my $line = $rep->('-');
+  my $hr = $line . "\n";
+  is $line->(10)            eq '-' x 10,
+  is $rep->($line->(10))(2) eq '-' x 20,
+  is $rep->($line)(3)(10)   eq '-' x 30,
+  is $hr->(20)              eq '-' x 20 . "\n",
 };
 
 use List::Util 'reduce';
 our ($a, $b);
 
 test 'chain', do {
-    my $chain = reduce {$a . $b} map &*, 0 .. 8;
-    my $link = $chain;
-    is $chain->(1)(2)(3)(4)(5)(6)(7)(8)(9) eq 123456789,
-    is $chain->(1..8)->(910) eq 12345678910,
-    do {
-        my $x = 9;
-        $link = $link->($x--) while ref $link;
-        is $link eq 987654321
-    }
+  my $chain = reduce {$a . $b} map &*, 0 .. 8;
+  my $link = $chain;
+  is $chain->(1)(2)(3)(4)(5)(6)(7)(8)(9) eq 123456789,
+  is $chain->(1..8)->(910) eq 12345678910,
+  do {
+      my $x = 9;
+      $link = $link->($x--) while ref $link;
+      is $link eq 987654321
+  }
 };
 
 test 'join', do {
-    my $join2   = &* . &*;
-    my $join4   = $join2 . $join2;
-    my $join8   = $join4 . $join4;
-    my $join16  = $join8 . $join8;
-    my $join16r = reduce {$a . $*} $*, 2 .. 16;
-    is $join2  ->(1 .. 2)   eq 12,
-    is $join4  ->(2)(4)(6)(8)   eq 2468,
-    is $join4  ->('a'..'d') eq 'abcd',
-    is $join16 ->(1 .. 16)  eq '12345678910111213141516',
-    is $join16r->(1 .. 16)  eq '12345678910111213141516',
+  my $join2   = &* . &*;
+  my $join4   = $join2 . $join2;
+  my $join8   = $join4 . $join4;
+  my $join16  = $join8 . $join8;
+  my $join16r = reduce {$a . $*} $*, 2 .. 16;
+  is $join2  ->(1 .. 2)     eq 12,
+  is $join4  ->(2)(4)(6)(8) eq 2468,
+  is $join4  ->('a'..'d')   eq 'abcd',
+  is $join16 ->(1 .. 16)    eq '12345678910111213141516',
+  is $join16r->(1 .. 16)    eq '12345678910111213141516',
 };
 
 test 'join bind', do {
-    my $join2   = &* .' '. &*;
-    my $join4   = $join2->($join2)($join2);
-    my $join8   = $join2->($join4)($join4);
-    my $join16  = $join2->($join8)($join8);
-    is $join2  ->(1 .. 2)   eq '1 2',
-    is $join4  ->(2)(4)(6)(8)   eq '2 4 6 8',
-    is $join4  ->('a'..'d') eq 'a b c d',
-    is $join16 ->(1 .. 16)  eq '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16',
+  my $join2   = &* .' '. &*;
+  my $join4   = $join2->($join2)($join2);
+  my $join8   = $join2->($join4, $join4);
+  my $join16  = $join2->($join8)($join8);
+  is $join2  ->(1 .. 2)     eq '1 2',
+  is $join4  ->(2)(4)(6)(8) eq '2 4 6 8',
+  is $join4  ->('a'..'d')   eq 'a b c d',
+  is $join16 ->(1 .. 16)    eq '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16',
 };
 
 test 'inc', do {
-    my $inc     = $* + 1;
-    my $inc_2   = $inc * 2;
-    my $inc_inc = $inc->($inc);
-    is $inc->(5)     == 6,
-    is $inc_2->(5)   == 12,
-    is $inc_inc->(5) == 7,
+  my $inc     = $* + 1;
+  my $inc_2   = $inc * 2;
+  my $inc_inc = $inc->($inc);
+  is $inc->(5)     == 6,
+  is $inc_2->(5)   == 12,
+  is $inc_inc->(5) == 7,
 };
+
+test 'assign &*', do {
+  (&*, my $x, &*, my $y) = 1 .. 4;
+  is $x == 2,
+  is $y == 4,
+  is &*->(5) == 5,
+};
+
+SKIP: {
+    test 'no assign $*', do {
+      eval {
+        ($*, my $x, $*, my $y) = 1 .. 4;
+        ** = \do{&*};
+        skip 'no assign $*: no readonly', 2
+      };
+      is $@ == qr/read.?only/,
+      is $*->(5) == 5,
+    }
+}
+
+test '$* ok',
+  is eval {$*->(3)} == 3;
+
+test 'array deref', do {
+  my $first  = &*->[0];
+  my $second = &*->[1];
+  my $last   = &*->[-1];
+  my $ntlast = &*->[-2];
+  my $slice  = [@{&*}[1, 2]];
+  my $slice2 = sub{\@_}->(@{&*}[3, 4]);
+  my $array  = [1 .. 5];
+  is $first ->($array) == 1,
+  is $second->($array) == 2,
+  is &*->[2]->($array) == 3,
+  is $last  ->($array) == 5,
+  is $ntlast->($array) == 4,
+  is $slice ->[0]->($array) == 2,
+  is $slice ->[1]->($array) == 3,
+  is $slice2->[0]->($array) == 4,
+  is $slice2->[1]->($array) == 5,
+};
+
+test 'array assign', do {
+  my $first = &*->[0];
+  my $last  = &*->[-1];
+  my $array = [1 .. 5];
+  $first->($array)  = 8;
+  $last->($array)   = 9;
+  &*->[2]->($array) = 0;
+  is $$array[0] == 8,
+  is $$array[4] == 9,
+  is $$array[2] == 0,
+  is "@$array" eq '8 2 0 4 9',
+};
+
+test 'array double', do {
+  my $first = &*->[0];
+  my $double = $*->[1][1];
+  my $double_app = $*->[1]($*->[1]);
+  my $array = [1, [2, 3], 4];
+  is &*->[0]   ($array) == 1,
+  is &*->[1][0]($array) == 2,
+  is &*->[1][1]($array) == 3,
+  is &*->[2]   ($array) == 4,
+  is $first->($array) == 1,
+  is $double->($array) == 3,
+  is $double_app->($array) == 3,
+};
+
+test 'array autoviv', do {
+  my $x;
+  &*->[1]($x) = 4;
+  is $$x[1] == 4
+};
+
+test 'array double autoviv', do {
+  my $first = &*->[0];
+  my $double = $first->($first);
+  my $array;
+  $double->($array) = 5;
+  &*->[1][0]($array) = 8;
+  is $$array[0][0] == 5,
+  is $$array[1][0] == 8,
+};
+
+test 'array deep', do {
+  &*->[0][0][0][0][0](my $array) = 3;
+  is $$array[0][0][0][0][0] == 3
+};
+
+test 'hash deref', do {
+  my $bob = &*->{bob};
+  my $alice = &*->{alice};
+  my $hash = {bob => 1, alice => 2};
+  is $bob->($hash) == 1,
+  is $alice->($hash) == 2,
+  is &*->{alice}->($hash) == 2,
+};
+
+test 'hash assign', do {
+  my $bob = &*->{bob};
+  my $hash = {bob => 1, alice => 2};
+  $bob->($hash) = 5;
+  &*->{eve}->($hash) = 10;
+  is $bob->($hash)  == 5,
+  is $hash->{alice} == 2,
+  is $hash->{eve}   == 10,
+};
+
+test 'hash double', do {
+  my $bob_has   = &*->{bob}{has};
+  my $alice     = &*->{alice};
+  my $has       = &*->{has};
+  my $alice_has = $has->($alice);
+  my $hash = {bob => {has => 5}, alice => {has => 7}};
+  is $bob_has->($hash) == 5,
+  is $hash->$bob_has == 5,
+  is $hash->$alice_has == 7,
+  is $hash->$alice->$has == 7,
+  is &*->{alice}{has}($hash) == 7
+};
+
+test 'hash autoviv', do {
+  my $x;
+  &*->{bob}($x) = 4;
+  is $$x{bob} == 4
+};
+
+test 'hash double autoviv', do {
+  my $eve  = &*->{eve};
+  my $has  = &*->{has};
+  my $eve_has = $has->($eve);
+  my $hash;
+  $eve_has->($hash) = 12;
+  &*->{bob}{has}($hash) = 8;
+  is $$hash{eve}{has} == 12,
+  is $$hash{bob}{has} == 8,
+};
+
+test 'hash deep', do {
+  &*->{a}{b}{c}{d}(my $hash) = 12;
+  is $$hash{a}{b}{c}{d} == 12
+};
+
+test 'array/hash deep', do {
+  &*->[0]{a}[1][2]{b}{c}(my $data) = 42;
+  is $$data[0]{a}[1][2]{b}{c} == 42
+}
+
